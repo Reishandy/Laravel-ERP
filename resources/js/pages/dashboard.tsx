e720d46b05367eaddd667927b1981fc62d1cc657
Muhammad Akbar Reishandy <akbar@reishandy.my.id>
2025-07-07 21:02:33 +0700

    feat: enhance dashboard layout with new CardType2Prototype and integrate product sales table

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -1,6 +1,7 @@
 import { Badge } from '@/components/ui/badge';
 import { Card, CardContent, CardFooter } from '@/components/ui/card';
 import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/react';
@@ -18,31 +19,31 @@ export default function Dashboard() {
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Dashboard" />
 
-            <div className="grid h-full gap-4 p-8 lg:grid-rows-4">
-                <div className="flex flex-col gap-4 lg:row-span-1 2xl:flex-row">
-                    <div className="flex size-full flex-col gap-4 lg:flex-row 2xl:flex-row">
+            <div className="grid h-full gap-4 p-8 lg:grid-rows-5">
+                <div className="row-span-1 flex flex-col gap-4 lg:row-span-1 2xl:flex-row">
+                    <div className="flex w-full flex-col gap-4 lg:flex-row 2xl:flex-row">
                         <CardType1Prototype />
                         <CardType1Prototype />
                     </div>
-                    <div className="flex size-full flex-col gap-4 lg:flex-row 2xl:flex-row">
+                    <div className="flex w-full flex-col gap-4 lg:flex-row 2xl:flex-row">
                         <CardType1Prototype />
                         <CardType1Prototype />
                     </div>
                 </div>
 
-                <div className="row-span-2">
+                <div className="row-span-3">
                     <div className="grid size-full grid-rows-2 gap-4 lg:grid-rows-4 2xl:grid-cols-4 2xl:grid-rows-2">
                         <div className="lg:row-span-3 2xl:col-span-3">
                             <PlaceholderPattern className="size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
                         </div>
                         <div className="flex size-full flex-col gap-4 lg:flex-row 2xl:row-span-3 2xl:flex-col">
-                            <PlaceholderPattern className="size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
-                            <PlaceholderPattern className="size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                            <CardType2Prototype />
+                            <CardType2Prototype />
                         </div>
                     </div>
                 </div>
 
-                <div className="row-span-1 flex flex-col gap-4 lg:flex-row">
+                <div className="row-span-2 flex flex-col gap-4 lg:flex-row">
                     <PlaceholderPattern className="size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
                     <PlaceholderPattern className="size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
                 </div>
@@ -53,7 +54,8 @@ export default function Dashboard() {
 
 function CardType1Prototype() {
     return (
-        <Card className="min-w-0 size-full justify-center bg-gradient-to-bl from-sidebar via-secondary to-sidebar rounded-2xl shadow-lg transition-transform hover:scale-105 hover:shadow-2xl border-0">
+        // border-0 bg-gradient-to-bl from-sidebar via-secondary to-sidebar
+        <Card className="h-fit w-full justify-center rounded-2xl bg-sidebar shadow-lg transition-transform hover:scale-105 hover:shadow-2xl">
             <CardContent className="p-8">
                 <div className="flex flex-col gap-y-4">
                     <div className="flex flex-row items-baseline justify-between">
@@ -62,15 +64,64 @@ function CardType1Prototype() {
                             <TrendingUp /> +20%
                         </Badge>
                     </div>
-                    <span className="text-4xl 2xl:text-2xl font-black break-all">Rp. 12.890.000.000,00</span>
+                    <span className="text-4xl font-black break-all 2xl:text-2xl">Rp. 12.890.000.000,00</span>
                 </div>
             </CardContent>
             <CardFooter>
                 <div className="flex flex-col gap-y-1">
-                    <span className="text-md text-muted-foreground flex flex-row gap-x-2">Trending up this month <TrendingUp /></span>
+                    <span className="text-md flex flex-row gap-x-2 text-muted-foreground">
+                        Trending up this month <TrendingUp />
+                    </span>
                     <span className="text-sm text-muted-foreground">Last month was $2.000.000,00</span>
                 </div>
             </CardFooter>
         </Card>
-    )
+    );
+}
+
+function CardType2Prototype() {
+    return (
+        <Card className="size-full justify-center rounded-2xl border-0 bg-sidebar shadow-lg transition-transform hover:scale-105 hover:shadow-2xl">
+            <CardContent className="p-8">
+                <div className="flex flex-col gap-y-6">
+                    <div className="flex flex-col items-center gap-2">
+                        <TrendingUp className="text-primary" size={32} />
+                        <span className="text-center text-xl font-bold">Top Product Sales This Month</span>
+                    </div>
+                    <Table>
+                        <TableCaption>Most sold products in June 2024</TableCaption>
+                        <TableHeader>
+                            <TableRow>
+                                <TableHead className="text-center">ID</TableHead>
+                                <TableHead>Product</TableHead>
+                                <TableHead className="text-end">Sold</TableHead>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            <TableRow className="transition hover:bg-secondary/30">
+                                <TableCell className="text-center font-semibold">P-01234</TableCell>
+                                <TableCell>
+                                    Washing Machine{' '}
+                                    <Badge className="ml-2" variant="default">
+                                        Top
+                                    </Badge>
+                                </TableCell>
+                                <TableCell className="text-end font-bold">20</TableCell>
+                            </TableRow>
+                            <TableRow className="transition hover:bg-secondary/30">
+                                <TableCell className="text-center font-semibold">P-05678</TableCell>
+                                <TableCell>Refrigerator</TableCell>
+                                <TableCell className="text-end font-bold">15</TableCell>
+                            </TableRow>
+                            <TableRow className="transition hover:bg-secondary/30">
+                                <TableCell className="text-center font-semibold">P-09876</TableCell>
+                                <TableCell>Microwave</TableCell>
+                                <TableCell className="text-end font-bold">10</TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                </div>
+            </CardContent>
+        </Card>
+    );
 }
