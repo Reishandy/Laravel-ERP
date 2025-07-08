5922f306888062e754d72143764aeebc58b11628
Muhammad Akbar Reishandy <akbar@reishandy.my.id>
2025-07-08 20:22:49 +0700

    fix: improve error handling for non-local environments

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -27,7 +27,7 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         $exceptions->respond(function (Response $response, Throwable $exception, Request $request) {
-            if (!config('app.debug')) {
+            if (! app()->environment(['local', 'testing']) && in_array($response->getStatusCode(), [500, 503, 404, 403])) {
                 return Inertia::render('error-page', ['status' => $response->getStatusCode()])
                     ->toResponse($request)
                     ->setStatusCode($response->getStatusCode());
