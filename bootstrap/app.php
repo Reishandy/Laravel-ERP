7a82dcdb426779ea3c6cf94686cf451e396eafde
Muhammad Akbar Reishandy <akbar@reishandy.my.id>
2025-07-08 20:29:27 +0700

    fix: improve error handling for non-local environments

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,8 +12,8 @@ use Symfony\Component\HttpFoundation\Response;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
@@ -27,10 +27,20 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         $exceptions->respond(function (Response $response, Throwable $exception, Request $request) {
+            $status = $response->getStatusCode();
+
+            // Exclude 3xx redirects and specific codes
+            if (
+                !config('app.debug') &&
+                $status < 400 && $status >= 300 || in_array($status, [401, 419])
+            ) {
+                return $response;
+            }
+
             if (!config('app.debug')) {
-                return Inertia::render('error-page', ['status' => $response->getStatusCode()])
+                return Inertia::render('error-page', ['status' => $status])
                     ->toResponse($request)
-                    ->setStatusCode($response->getStatusCode());
+                    ->setStatusCode($status);
             }
 
             return $response;
